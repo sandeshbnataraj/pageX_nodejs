8dc8bed296e85d1a745fafc8b316e45fda15fbf8
Atif <Atif@DESKTOP-VVJ1ITJ>
2019-10-19 07:42:17 +0500

    removing extra code

--- src/components/profileCenter/AddCollectionModel.js
+++ src/components/profileCenter/AddCollectionModel.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Modal, Card, CardImg, Row, Container, Col, Form, Button, CardColumns } from "react-bootstrap";
+import { Modal } from "react-bootstrap";
 
 
 import "./AddCollectionModel.scss";
@@ -11,85 +11,8 @@ export default class AddCollectionModel extends Component {
         onHide: PropTypes.func.isRequired
     };
 
-    renderCards = () =>{
-        const { show, onHide, workPublication } = this.props;
-        let cards = {
-            firstColumn : [],
-            secondColumn : [],
-            thirdColumn : [],
-        }
-
-        let i = 0;
-
-        workPublication.filter((v) => (v.publication_type == 2)).map(w => {
-            switch(i%3){
-                case 0:{
-                    cards['firstColumn'].push(
-                        <Card  style={{ margin: '15px', textAlign: 'center',float:'left' }}>
-                            <Form.Check className="checkbox" type="checkbox" />
-                            <CardImg top width="60%" src='http://deveycon.herokuapp.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBFdz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--2fc75768999fa25341695f1f66af2d43be9f47d8/index.jpg' alt="Card image cap" />
-                            {/* <Card.Header><input type="checkbox" /></Card.Header> */}
-                            <Card.Body className="bodycss" style={{maxWidth:'150px'}}>
-                                {/* <CardSubtitle>Card subtitle</CardSubtitle> */}
-                                <Card.Text dangerouslySetInnerHTML={{__html:w.publication_text}}/>
-                                    {/* {w.publication_text} */}
-                                {/* <Card.Footer><input type="checkbox" /></Card.Footer> */}
-                                {/* <Button>Button</Button> */}
-                            </Card.Body>
-                        </Card>
-                    );
-                    break;
-                }
-                case 1:{
-
-                    cards['secondColumn'].push(
-                        <Card  style={{ margin: '15px', textAlign: 'center',float:'left' }}>
-                            <Form.Check className="checkbox" type="checkbox" />
-                            <CardImg top width="60%" src='http://deveycon.herokuapp.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBFdz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--2fc75768999fa25341695f1f66af2d43be9f47d8/index.jpg' alt="Card image cap" />
-                            {/* <Card.Header><input type="checkbox" /></Card.Header> */}
-                            <Card.Body style={{maxWidth:'150px'}}>
-                                {/* <CardSubtitle>Card subtitle</CardSubtitle> */}
-                                <Card.Text dangerouslySetInnerHTML={{__html:w.publication_text}}/>
-                                    {/* {w.publication_text} */}
-                                {/* <Card.Footer><input type="checkbox" /></Card.Footer> */}
-                                {/* <Button>Button</Button> */}
-                            </Card.Body>
-                        </Card>
-                    );
-                    break;
-
-                }
-                case 2:{
-
-                    cards['thirdColumn'].push(
-                        <Card  style={{ margin: '15px', textAlign: 'center',float:'left' }}>
-                            <Form.Check className="checkbox" type="checkbox" />
-                            <CardImg top width="60%" src='http://deveycon.herokuapp.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBFdz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--2fc75768999fa25341695f1f66af2d43be9f47d8/index.jpg' alt="Card image cap" />
-                            {/* <Card.Header><input type="checkbox" /></Card.Header> */}
-                            <Card.Body style={{maxWidth:'150px'}}>
-                                {/* <CardSubtitle>Card subtitle</CardSubtitle> */}
-                                <Card.Text dangerouslySetInnerHTML={{__html:w.publication_text}}/>
-                                    {/* {w.publication_text} */}
-                                {/* <Card.Footer><input type="checkbox" /></Card.Footer> */}
-                                {/* <Button>Button</Button> */}
-                            </Card.Body>
-                        </Card>
-                    );
-                    break;
-
-                }
-            }
-            i++;
-            
-        });
-        console.log(cards)
-        return cards
-    }
-
     render() {
         const { show, onHide, workPublication } = this.props;
-        console.log(workPublication);
-        let data = this.renderCards()
         return (
             <Modal
                 show={show}
